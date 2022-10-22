63e92f8ebbe86dec9e7717fbd7d9dfeb94080443
SLFireFLash <sllahiruprasanna@gmail.com>
2022-10-22 12:08:19 +0530

    game logic complete

--- js/script.js
+++ js/script.js
@@ -2,7 +2,12 @@ const boxes =document.querySelectorAll(".box");
 const mole = document.querySelector(".mole");
 
 const score = document.getElementById("score-no");
-const timeLeft = document.getElementById("timeLeft-no");
+const timeLeft = document.getElementById("TimeLeft-no");
+
+let result =0;
+let hitPos;
+let timeNow =5;
+let timer =null;
 
 function randomBox(){
     boxes.forEach(box =>{
@@ -10,14 +15,44 @@ function randomBox(){
     })
     let randPos = boxes[Math.floor(Math.random()*9)];
     randPos.classList.add('mole');
+    hitPos =randPos.id;
     
 }
 
+boxes.forEach(box =>{
+    box.addEventListener('mousedown',()=>{
+        if(box.id == hitPos){
+            result++;
+            score.textContent =result
+        }
+    })
+})
+
+
+
 function autoChangeBox(){
-    let timer =null;
-    timer = setInterval(randomBox,500);
-    console.log(timer)
+    timer = setInterval(randomBox,2000);
     
 }
+function countDonw(){
+    timeNow--
+    timeLeft.textContent = timeNow
+
+    if(timeNow ==0)
+    {
+        clearInterval(countDonwId);
+        clearInterval(timer)
+        hitPos = -1
+        boxes.forEach(box =>{
+            box.classList.remove('mole');
+        })
+        alert("Game Over Your score Is: "+ result);
+
+    }
+
+}
+
+let countDonwId =setInterval(countDonw,1000);
+
 
 autoChangeBox()
